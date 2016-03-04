b65f87d37585cde8381b4cd1099c3a42cee3dfdf
Emanuel Moser <emanuel.moser@student.tugraz.at>
2016-03-04 19:23:52 +0100

    + design coding

--- install_fresh.sh
+++ install_fresh.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+#design
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+red='tput setaf 1'
+green='tput setaf 2'
 
 #apt-get update
 echo "Fetching new package lists"
 
 declare -a INSTALL
-INSTALL=( unrar unzip )
+INSTALL=( unrar unzip git)
 
 for i in "${INSTALL[@]}"
 do
  if [[ $(dpkg-query -W -f='${Status}' $i 2>/dev/null | grep -c "ok installed") -eq 0 ]]; then
-  echo "$i not installes"
+  echo "${bold}$i${normal} will be installed"
+  apt-get install $i
  elif [[ $(dpkg-query -W -f='${Status}' $i 2>/dev/null | grep -c "ok installed") -eq 1 ]]; then
-  echo "$i installed" 
+  echo "${bold}$i${normal} already installed installed"
  fi
 done
 
